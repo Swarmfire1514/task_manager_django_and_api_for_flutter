e597e4e10c39c6587cbfb31b2fee0d426e11303b
Swarmfire1514 <sthamahes85@gmail.com>
2026-01-11 17:09:40 +0545

    minor bug in search_fields: admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -5,5 +5,5 @@ from .models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id','title','description','status','priority','due_date','category','created_at','updated_at')
-    search_fields = ('category__user__username')
+    search_fields = ('category__user__username','category')
     list_filter = ('category__user','title','priority','category','status')
